42aed4267aac4a41884c9da5bb50915c410d1d91
Ronald-Manzano2 <71304945+Ronald-Manzano2@users.noreply.github.com>
2022-07-08 09:50:23 -0600

    Add files via upload

---
+++ Guía-1/Analisis_resultados/ejercicio3/js/tmp.js
@@ -0,0 +1,4 @@
+var cel = prompt("Digite la tempeatura en grados celsius que desea convertir");
+var far = (Number(cel)*(1.8)) + 32;
+
+document.write("<h1>Los "+ cel +"° celsius equivalen a "+ far +"°F.</h1>");
