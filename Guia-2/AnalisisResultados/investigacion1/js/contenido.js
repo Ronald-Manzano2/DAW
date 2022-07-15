b35fb775cb20fea5601a57f626d4f6eda96fcbd0
Ronald-Manzano2 <71304945+Ronald-Manzano2@users.noreply.github.com>
2022-07-15 08:24:59 -0600

    Add files via upload

---
+++ Guia-2/AnalisisResultados/investigacion1/js/contenido.js
@@ -0,0 +1,5 @@
+document.write("<h1>Objeto Math</h>");
+document.write("<h2>Función abs()</h2>");
+document.write("<p>La función Math.abs() devuelve el valor absoluto de un número. Es decir, devuelve x si x es positivo o cero, y la negación de x si x es negativo.</p>");
+document.write("<h2>Ejemplo</h2>");
+document.write("<label for='num'>Escriba un número entero: </label><input type='text' id='num'><input type='submit' value='comprobar'>");
